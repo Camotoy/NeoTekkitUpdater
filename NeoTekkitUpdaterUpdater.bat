2abbf356bb6687d8d2b4e001b602f38472a4614b
DoctorMacc <20743703+DoctorMacc@users.noreply.github.com>
2017-08-19 08:02:20 -0400

    Show completion of update

--- NeoTekkitUpdaterUpdater.bat
+++ NeoTekkitUpdaterUpdater.bat
@@ -1,2 +1,4 @@
+@echo off
 powershell -Command "Invoke-WebRequest https://github.com/DoctorMacc/NeoTekkitUpdater/raw/master/NeoTekkitUpdater.bat -OutFile NeoTekkitUpdater.bat"
+echo Update complete.
 call NeoTekkitUpdater.bat
