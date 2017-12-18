61abd2f04af28aa74313898f7886fc65a2d96e2c
DoctorMacc <20743703+DoctorMacc@users.noreply.github.com>
2017-12-18 18:21:50 -0500

    Add more variables and comments.

--- NeoTekkitUpdater.bat
+++ NeoTekkitUpdater.bat
@@ -1,18 +1,25 @@
 @echo off
+:: Set variables.
 set batchversion=0.3
+set txtUrl=https://github.com/DoctorMacc/NeoTekkitUpdater/raw/master/NeoTekkitVersion.txt
+set updUrl=https://github.com/DoctorMacc/NeoTekkitUpdater/raw/master/NeoTekkitUpdaterUpdater.bat
+set zipUrl=
 title Neo Tekkit Updater v%batchversion%
 
+:: Clean up.
 if exist NeoTekkitUpdaterUpdater.bat (del NeoTekkitUpdaterUpdater.bat)
 
-powershell -Command "Invoke-WebRequest https://github.com/DoctorMacc/NeoTekkitUpdater/raw/master/NeoTekkitVersion.txt -OutFile NeoTekkitVersion.txt"
+:: Check for an update to the script.
+powershell -Command "Invoke-WebRequest %txtUrl% -OutFile NeoTekkitVersion.txt"
 set /p version=<NeoTekkitVersion.txt
-if not %version%==%batchversion% (powershell -Command "Invoke-WebRequest https://github.com/DoctorMacc/NeoTekkitUpdater/raw/master/NeoTekkitUpdaterUpdater.bat -OutFile NeoTekkitUpdaterUpdater.bat"
+if not %version%==%batchversion% (powershell -Command "Invoke-WebRequest %updUrl% -OutFile NeoTekkitUpdaterUpdater.bat"
 	del NeoTekkitVersion.txt
 	echo Updating the updater...
 	call NeoTekkitUpdaterUpdater.bat
 	exit)
 del NeoTekkitVersion.txt
 
+:: Actually update the modpack.
 if exist %cd%\NeoTekkit.txt (set /p forge=<NeoTekkit.txt
 	set txt=1)
 if not exist %cd%\NeoTekkit.txt (echo Drag your Minecraft MODS directory to the prompt.
@@ -20,7 +27,7 @@ if not exist %cd%\NeoTekkit.txt (echo Drag your Minecraft MODS directory to the 
 	echo.%forge%>NeoTekkit.txt
 	set txt=0)
 echo Downloading NeoTekkit.zip to %cd%...
-powershell -Command "Invoke-WebRequest http://76.190.144.116/NeoTekkit.zip -OutFile NeoTekkit.zip"
+powershell -Command "Invoke-WebRequest %zipUrl% -OutFile NeoTekkit.zip"
 if not exist %forge%\..\mods_old\ (mkdir %forge%\..\mods_old\)
 echo Moving old mods folder to %forge%\..\mods_old...
 move %forge%\* %forge%\..\mods_old\
